4d03bf95bed0c3233426ef2f287388d63ce9a315
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2017-12-31 12:06:28 +0530

    Removed debug message

--- service/transport.go
+++ service/transport.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"github.com/go-kit/kit/endpoint"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
@@ -39,7 +38,6 @@ func MakeSendEmailHandler(e endpoint.Endpoint, r *mux.Router) {
 func decodeSendEmailRequest(_ context.Context, r *http.Request) (interface{}, error) {
 	var request sendEmailRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		fmt.Println(err)
 		return nil, ErrJSONUnmarshall
 	}
 	return request, nil
